497155d865718042da61064f93a143acec50bd78
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2017-06-07 13:47:40 +0200

    generate build task

--- src/representation.js
+++ src/representation.js
@@ -1,8 +1,25 @@
+import * as fs from 'fs';
 import _ from 'lodash';
+import logWith from 'log-with';
+import mkdirp from 'mkdirp';
+import * as path from 'path';
+
+const logger = logWith(module);
 
 class Representation {
   constructor() {
     this.sources = [];
+  }
+
+  static write(data, file) {
+    const filePath = path.join(__dirname, file);
+    logger.debug('File created at', filePath);
+    mkdirp(path.dirname(filePath), (err) => {
+      if (err) {
+        throw err;
+      }
+      fs.writeFileSync(filePath, JSON.stringify(data, null, 4));
+    });
   }
 
   addSource(source) {
@@ -20,6 +37,18 @@ class Representation {
     });
     this.payload = _.compact(await Promise.all(sources));
     return this;
+  }
+
+  async build() {
+    await this.load();
+    return this;
+  }
+
+  async generate(file) {
+    await this.build();
+    const result = this.payload || {};
+    Representation.write(result, file);
+    return result;
   }
 }
 
