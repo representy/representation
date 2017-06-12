6e1dae1f1f845881bec187c0a3df81777c44271d
Salim KAYABAŞI <salim.kayabasi@gmail.com>
2017-06-12 15:24:02 +0200

    update depdendencies

--- test/representation.test.js
+++ test/representation.test.js
@@ -5,6 +5,15 @@ const token = process.env.GITHUB_TOKEN;
 describe('Representation', () => {
   test('generate', async () => {
     const representation = new Representation({
+      json: true,
+      file: 'salim.json',
+      profile: {
+        name: 'Salim KAYABASI',
+        photo: 'https://lh3.googleusercontent.com/B2G2t605HLpRlS6dEgW96LwekkQaNXvq9sRuBBijqq4tdQ0k09bnWKS0Bq8CNyqwuMYbm2uwbEaB62E=w5760-h3600-rw-no',
+        title: 'Software Engineer',
+        location: 'in Berlin, Germany',
+        describe: 'life is almost perfect',
+      },
       tokens: {
         github: token,
       },
